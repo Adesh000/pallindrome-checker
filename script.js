23f57e487d3550a629fed26c35d65a6d083d95d6
Adesh Nayak <adeshnayak6@gmail.com>
2022-11-13 18:06:48 +0530

    almost done

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let input = document.getElementById("string")
-let Btn = document.querySelector("button")
-let result = document.querySelector(".main-container > p")
+let inputStr = document.getElementById("string");
+let Btn = document.querySelector("button");
+let result = document.querySelector(".main-container > p");
 
+
+const pallChecker = () => {
+    let textStr = inputStr.value;
+    let revStr = textStr.split("").reverse().join("");
+    if (textStr === revStr) {
+        result.innerHTML = "The given string is Pallindrome";
+    } else {
+        result.innerHTML = "The given string is not Pallindrome";
+    }
+};
+
+
+Btn.addEventListener("click", pallChecker);
